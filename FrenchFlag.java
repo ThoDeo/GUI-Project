4109d795329303f4998e9fe21d1c6c0e0bed4579
Thomas Deolall <>
2022-10-27 17:49:05 +0000

    Initial commit

---
+++ FrenchFlag.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+public class FrenchFlag {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("French Flag");
+        frame.setSize(300,200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Panel eastPanel = new Panel();
+        Panel centerPanel = new Panel();
+        Panel westPanel = new Panel();
+        westPanel.setBackground(Color.blue);
+        eastPanel.setBackground(Color.red);
+        centerPanel.setBackground(Color.white);
+        Container pane = frame.getContentPane();
+        pane.setLayout(new GridLayout(1, 3));
+        pane.add(westPanel);
+        pane.add(centerPanel);
+        pane.add(eastPanel);
+        frame.setVisible(true);
+    }
+}
